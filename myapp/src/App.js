c38fa7472ea551facaf798a0bc9f4a3fce1f8a39
ZaynahA <zaynah.ahmad@cdpipelinedevshop.com>
2022-07-19 12:30:46 +0000

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Login from "./components/Userstory";
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Login></Login>
+       
+        
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/Userstory.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import { Paper, Avatar, TextField, Button } from "@mui/material";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+const Login=()=> {
+    const paperStyle={
+        padding: "20px",
+        height: "70vh",
+        width: 280,
+        margin: "20px auto"  
+    }
+    const avatarStyle={
+        backgroundColor:"black"
+    }
+    const btnstyle={
+        margin:"8px 0"
+    }
+
+
+
+
+    return(
+        <Grid>
+             <Paper elevation={10} style={paperStyle}>
+                 <Grid align="center">
+                <Avatar style={avatarStyle}><ShoppingCartIcon></ShoppingCartIcon></Avatar>
+                <h2>Welcome To your Online Shopping Cart</h2>
+                </Grid>
+
+                <TextField label="Beans" placeholder="Add or Subtract" fullWidth required></TextField>
+                <TextField label="Soup" placeholder="Add or Subtract" fullWidth required></TextField>
+                <TextField label="Cheese" placeholder="Add or Subtract" fullWidth required></TextField>
+                <TextField label="Chicken" placeholder="Add or Subtract" fullWidth required></TextField>
+                <TextField label="Vegetables" placeholder="Add or Subtract" fullWidth required></TextField>
+                
+        
+                <Button
+                    type="Submit"
+                    color="primary"
+                    variant="contained"
+                    style={btnstyle}
+                    fullWidth
+                    >
+                        Donate Items
+                     </Button>
+                     
+<p> Thank you so much for Donating to the Hope Center!</p>
+                     
+                </Paper>
+                </Grid>
+
+    )
+}
+export default Login;
